c43f6feff07823cf4d2123103a5ef30dac778598
Emre <emredirek1905@gmail.com>
2024-08-23 22:34:33 +0300

    insert-Update-Delete

--- RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -13,6 +13,30 @@ namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
             _context = context;
         }
 
+        public async void CreateCategory(CreateCategoryDto categoryDto)
+        {
+            string query = "insert into Category(CategoryName,CategoryStatus) Values (@categoryName,@categoryStatus)";
+            var paremeters = new DynamicParameters();
+            paremeters.Add("@categoryName", categoryDto.CategoryName);
+            paremeters.Add("@categoryStatus", true);
+            using (var con = _context.CreateConnection())
+            {
+                await con.ExecuteAsync(query,paremeters);
+            }
+
+        }
+
+        public async void DeleteCategory(int id)
+        {
+            string query = "Delete From Category Where CategoryID=@categoryID";
+            var paremeters = new DynamicParameters();
+            paremeters.Add("@categoryID", id); 
+            using (var con = _context.CreateConnection())
+            {
+                await con.ExecuteAsync(query, paremeters);
+            }
+        }
+
         public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
         {
             string query = "Select * From Category";
@@ -20,6 +44,19 @@ namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
             {
                 var values = await connection.QueryAsync<ResultCategoryDto>(query);
                 return values.ToList();
+            }
+        }
+
+        public async void UpdateCategory(UpdateCategoryDto categoryDto)
+        {
+            string query = "update Category Set CategoryName=@categoryName,CategoryStatus=@categoryStatus Where CategoryID=@categoryID";
+            var paremeters = new DynamicParameters();
+            paremeters.Add("@categoryName", categoryDto.CategoryName);
+            paremeters.Add("@categoryStatus", categoryDto.CategoryStatus);
+            paremeters.Add("@categoryID", categoryDto.CategoryId);
+            using (var con = _context.CreateConnection())
+            {
+                await con.ExecuteAsync(query, paremeters);
             }
         }
     }
