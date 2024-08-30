217cb69a779734693eeacd514c7f206bd314b775
Emre <emredirek1905@gmail.com>
2024-08-31 01:57:42 +0300

    xxx

---
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responeseMessage = await client.GetAsync("https://localhost:44319/api/Categories");
+
+            if (responeseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responeseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        } 
+    }
+}
