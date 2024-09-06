8ffb3d251ddb9fd28db819a34920db8ddbfbd418
Emre <emredirek1905@gmail.com>
2024-09-06 16:13:03 +0300

    LoginJWTCheck

---
+++ RealEstate_Dapper_Api/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.LoginDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Tools; 
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(CreateLoginDto _createLoginDto)
+        {
+            string query = "select * from AppUser where Username=@username and Password=@password";
+            var parameters = new DynamicParameters();
+            parameters.Add("@username", _createLoginDto.UserName);
+            parameters.Add("@password", _createLoginDto.Password);
+            using (var con = _context.CreateConnection())
+            {
+                var values = await con.QueryFirstOrDefaultAsync<CreateLoginDto>(query,parameters);
+                var values2 = await con.QueryFirstOrDefaultAsync<ResultLoginIdDto>(query, parameters);
+                if (values !=null)
+                {
+                    GetCheckAppUserViewModel viewModel = new GetCheckAppUserViewModel();
+                    viewModel.UserName = values.UserName;
+                    viewModel.id = values2.UserId;
+                    var _token = JwtTokenGenerator.GenerateToken(viewModel);
+                    return Ok(_token);
+                }                    
+                else
+                    return Ok("Başarısızzz!!!!!!!");
+
+
+            }
+
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/LoginDtos/ResultLoginIdDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_Dapper_Api.LoginDtos
+{
+    public class ResultLoginIdDto
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
