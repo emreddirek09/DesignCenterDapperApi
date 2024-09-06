1215b58188d916bfa2039418739137b940ffa05a
Emre <emredirek1905@gmail.com>
2024-09-06 11:11:12 +0300

    JWT-Json Web Token yapılandırılması - step 1

---
+++ RealEstate_Dapper_Api/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "https://localhost";
+        public const string ValidIssuer = "https://localhost";
+        public const string Key = "EmreCemreDirek...1234567890*-#^";
+        public const int Expire = 5; //5dk
+
+    }
+}
---
+++ RealEstate_Dapper_Api/Tools/TokenResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+    public class TokenResponseViewModel
+    {
+        public TokenResponseViewModel(string _token, DateTime _expireDate)
+        {
+            Token = _token;
+            ExpireDate = _expireDate;
+        }
+
+        public string Token { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
